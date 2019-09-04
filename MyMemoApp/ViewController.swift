c01a6718eb5c836e1ab74ffcd41952e1467182e1
yukiii08 <going.my.wayx8@gmail.com>
2019-09-04 15:01:29 +0900

    Initial Commit

---
+++ MyMemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemoApp
+//
+//  Created by 久野　由貴　 on 2019/09/04.
+//  Copyright © 2019 hisano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
